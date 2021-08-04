9cc436519465b355aaa0231563fc221b0e9fcbc1
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-08-04 12:38:25 -0400

    First commit: Read in basics - no block closure

---
+++ celeri.py
@@ -0,0 +1,150 @@
+import copy
+import numpy as np
+from pyproj import Geod
+
+import celeri
+
+
+RADIUS_EARTH = np.float64(6371e3)  # m
+GEOID = Geod(ellps="WGS84")
+
+
+def sph2cart(lon, lat, radius):
+    x = radius * np.cos(np.deg2rad(lat)) * np.cos(np.deg2rad(lon))
+    y = radius * np.cos(np.deg2rad(lat)) * np.sin(np.deg2rad(lon))
+    z = radius * np.sin(np.deg2rad(lat))
+    return x, y, z
+
+
+def process_station(station, command):
+    if command["unit_sigmas"] == "yes":  # Assign unit uncertainties, if requested
+        station.east_sig = np.ones_like(station.east_sig)
+        station.north_sig = np.ones_like(station.north_sig)
+        station.up_sig = np.ones_like(station.up_sig)
+
+    station["dep"] = np.zeros_like(
+        station.lon
+    )  # Add a "dep" field of all zeros, to be used with project_tri_coords
+    station["x"], station["y"], station["z"] = celeri.sph2cart(
+        station.lon, station.lat, celeri.RADIUS_EARTH
+    )
+    station = station[station.tog == True]  # Keep only the stations that are toggled on
+    return station
+
+
+def locking_depth_manager(segment, command):
+    """
+    This function assigns the locking depths given in the command file to any
+    segment that has the same locking depth flag.  Segments with flag =
+    0, 1 are untouched.
+    """
+    segment = segment.copy(deep=True)
+    segment.locking_depth.values[segment.locking_depth_flag == 2] = command[
+        "locking_depth_flag2"
+    ]
+    segment.locking_depth.values[segment.locking_depth_flag == 3] = command[
+        "locking_depth_flag3"
+    ]
+    segment.locking_depth.values[segment.locking_depth_flag == 4] = command[
+        "locking_depth_flag4"
+    ]
+    segment.locking_depth.values[segment.locking_depth_flag == 5] = command[
+        "locking_depth_flag5"
+    ]
+
+    if command["locking_depth_override_flag"] == "yes":
+        segment.locking_depth.values = command["locking_depth_override_value"]
+    return segment
+
+
+def order_endpoints_sphere(segment):
+    """
+    Endpoint ordering function, placing west point first.
+    This converts the endpoint coordinates from spherical to Cartesian,
+    then takes the cross product to test for ordering (i.e., a positive z
+    component of cross(point1, point2) means that point1 is the western
+    point). This method works for both (-180, 180) and (0, 360) longitude
+    conventions.
+    """
+    segment_copy = copy.deepcopy(segment)
+    x1, y1, z1 = celeri.sph2cart(segment.lon1, segment.lat1, 1)
+    x2, y2, z2 = celeri.sph2cart(segment.lon1, segment.lat2, 1)
+    for i in range(x1.size):
+        cross_product = np.cross(
+            [x1[i], y1[i], z1[i]], [x2[i], y2[i], z2[i]]
+        )  # TODO: Need to work on this!!!
+        if cross_product[2] <= 0:
+            segment_copy.lon1.values[i] = segment.lon2.values[i]
+            segment_copy.lat1.values[i] = segment.lat2.values[i]
+            segment_copy.lon2.values[i] = segment.lon1.values[i]
+            segment_copy.lat2.values[i] = segment.lat1.values[i]
+    return segment_copy
+
+
+def segment_centroids(segment):
+    """Calculate segment centroids."""
+    segment["centroid_x"] = np.zeros_like(segment.lon1)
+    segment["centroid_y"] = np.zeros_like(segment.lon1)
+    segment["centroid_z"] = np.zeros_like(segment.lon1)
+    segment["centroid_lon"] = np.zeros_like(segment.lon1)
+    segment["centroid_lat"] = np.zeros_like(segment.lon1)
+
+    for i in range(len(segment)):
+        segment_forward_azimuth, _, _ = celeri.GEOID.inv(
+            segment.lon1[i], segment.lat1[i], segment.lon2[i], segment.lat2[i]
+        )
+        segment_down_dip_azimuth = segment_forward_azimuth + 90.0 * np.sign(
+            np.cos(np.deg2rad(segment.dip[i]))
+        )
+        azx = (segment.y2[i] - segment.y1[i]) / (segment.x2[i] - segment.x1[i])
+        azx = np.arctan(-1.0 / azx)  # TODO: FIX THIS VARIABLE NAME
+        segment.centroid_z.values[i] = (
+            segment.locking_depth[i] - segment.burial_depth[i]
+        ) / 2.0
+        segment_down_dip_distance = segment.centroid_z[i] / np.abs(
+            np.tan(np.deg2rad(segment.dip[i]))
+        )
+        (
+            segment.centroid_lon.values[i],
+            segment.centroid_lat.values[i],
+            _,
+        ) = celeri.GEOID.fwd(
+            segment.mid_lon[i],
+            segment.mid_lat[i],
+            segment_down_dip_azimuth,
+            segment_down_dip_distance,
+        )
+        segment.centroid_x.values[i] = segment.mid_x[i] + np.sign(
+            np.cos(np.deg2rad(segment.dip[i]))
+        ) * segment_down_dip_distance * np.cos(azx)
+        segment.centroid_y.values[i] = segment.mid_y[i] + np.sign(
+            np.cos(np.deg2rad(segment.dip[i]))
+        ) * segment_down_dip_distance * np.sin(azx)
+    segment.centroid_lon.values[segment.centroid_lon < 0.0] += 360.0
+    return segment
+
+
+def process_segment(segment, command):
+    segment = celeri.order_endpoints_sphere(segment)
+    segment["x1"], segment["y1"], segment["z1"] = celeri.sph2cart(
+        segment.lon1, segment.lat1, celeri.RADIUS_EARTH
+    )
+    segment["x2"], segment["y2"], segment["z2"] = celeri.sph2cart(
+        segment.lon2, segment.lat2, celeri.RADIUS_EARTH
+    )
+    segment["mid_lon"] = np.zeros_like(segment.lon1)
+    segment["mid_lat"] = np.zeros_like(segment.lon1)
+
+    for i in range(len(segment)):
+        segment.mid_lon.values[i], segment.mid_lat.values[i] = celeri.GEOID.npts(
+            segment.lon1[i], segment.lat1[i], segment.lon2[i], segment.lat2[i], 1
+        )[0]
+    segment.mid_lon.values[segment.mid_lon < 0.0] += 360.0
+
+    segment["mid_x"], segment["mid_y"], segment["mid_z"] = celeri.sph2cart(
+        segment.mid_lon, segment.mid_lat, celeri.RADIUS_EARTH
+    )
+    segment = celeri.locking_depth_manager(segment, command)
+    # segment.locking_depth.values = PatchLDtoggle(segment.locking_depth, segment.patch_file_name, segment.patch_flag, Command.patchFileNames) % Set locking depth to zero on segments that are associated with patches # TODO: Write this after patches are read in.
+    segment = celeri.segment_centroids(segment)
+    return segment
