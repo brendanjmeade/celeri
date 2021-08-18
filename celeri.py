3d9641e666ce4544943faaffa9855212b002bfa1
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-08-17 22:04:17 -0400

    Added KM2M and formatting

--- celeri.py
+++ celeri.py
@@ -13,6 +13,7 @@ import celeri
 
 
 GEOID = Geod(ellps="WGS84")
+KM2M = 1.0e3
 RADIUS_EARTH = np.float64((GEOID.a + GEOID.b) / 2)
 
 
@@ -30,40 +31,60 @@ def read_data(command_file_name):
     block = block.loc[:, ~block.columns.str.match("Unnamed")]
 
     # Read mesh data
-    with open(command["mesh_param_file_name"], 'r') as f:
+    with open(command["mesh_param_file_name"], "r") as f:
         mesh_param = json.load(f)
     meshes = {}
     for i in range(len(mesh_param)):
         meshes[i] = meshio.read(mesh_param[i]["mesh_filename"])
         meshes[i].verts = meshes[i].get_cells_type("triangle")
         # Expand mesh coordinates
-        meshes[i].lon1 = meshes[i].points[meshes[i].verts[:,0],0]
-        meshes[i].lon2 = meshes[i].points[meshes[i].verts[:,1],0]
-        meshes[i].lon3 = meshes[i].points[meshes[i].verts[:,2],0]
-        meshes[i].lat1 = meshes[i].points[meshes[i].verts[:,0],1]
-        meshes[i].lat2 = meshes[i].points[meshes[i].verts[:,1],1]
-        meshes[i].lat3 = meshes[i].points[meshes[i].verts[:,2],1]
-        meshes[i].dep1 = meshes[i].points[meshes[i].verts[:,0],2]
-        meshes[i].dep2 = meshes[i].points[meshes[i].verts[:,1],2]
-        meshes[i].dep3 = meshes[i].points[meshes[i].verts[:,2],2]
+        meshes[i].lon1 = meshes[i].points[meshes[i].verts[:, 0], 0]
+        meshes[i].lon2 = meshes[i].points[meshes[i].verts[:, 1], 0]
+        meshes[i].lon3 = meshes[i].points[meshes[i].verts[:, 2], 0]
+        meshes[i].lat1 = meshes[i].points[meshes[i].verts[:, 0], 1]
+        meshes[i].lat2 = meshes[i].points[meshes[i].verts[:, 1], 1]
+        meshes[i].lat3 = meshes[i].points[meshes[i].verts[:, 2], 1]
+        meshes[i].dep1 = meshes[i].points[meshes[i].verts[:, 0], 2]
+        meshes[i].dep2 = meshes[i].points[meshes[i].verts[:, 1], 2]
+        meshes[i].dep3 = meshes[i].points[meshes[i].verts[:, 2], 2]
         meshes[i].centroids = np.mean(meshes[i].points[meshes[i].verts, :], axis=1)
         # Cartesian coordinates in meters
         meshes[i].x1, meshes[i].y1, meshes[i].z1 = celeri.sph2cart(
-            meshes[i].lon1, meshes[i].lat1, celeri.RADIUS_EARTH+1000.*meshes[i].dep1
-            )
+            meshes[i].lon1,
+            meshes[i].lat1,
+            celeri.RADIUS_EARTH + KM2M * meshes[i].dep1,
+        )
         meshes[i].x2, meshes[i].y2, meshes[i].z2 = celeri.sph2cart(
-            meshes[i].lon2, meshes[i].lat2, celeri.RADIUS_EARTH+1000.*meshes[i].dep2)
+            meshes[i].lon2,
+            meshes[i].lat2,
+            celeri.RADIUS_EARTH + KM2M * meshes[i].dep2,
+        )
         meshes[i].x3, meshes[i].y3, meshes[i].z3 = celeri.sph2cart(
-            meshes[i].lon3, meshes[i].lat3, celeri.RADIUS_EARTH+1000.*meshes[i].dep3)
+            meshes[i].lon3,
+            meshes[i].lat3,
+            celeri.RADIUS_EARTH + KM2M * meshes[i].dep3,
+        )
         # Cross products for orientations
-        tri_leg1 = np.transpose([np.deg2rad(meshes[i].lon2-meshes[i].lon1), 
-             np.deg2rad(meshes[i].lat2-meshes[i].lat1), 
-             (1+1000.*meshes[i].dep2/celeri.RADIUS_EARTH)-(1+1000.*meshes[i].dep1/celeri.RADIUS_EARTH)])
-        tri_leg2 = np.transpose([np.deg2rad(meshes[i].lon3-meshes[i].lon1), 
-             np.deg2rad(meshes[i].lat3-meshes[i].lat1), 
-             (1+1000.*meshes[i].dep3/celeri.RADIUS_EARTH)-(1+1000.*meshes[i].dep1/celeri.RADIUS_EARTH)])
+        tri_leg1 = np.transpose(
+            [
+                np.deg2rad(meshes[i].lon2 - meshes[i].lon1),
+                np.deg2rad(meshes[i].lat2 - meshes[i].lat1),
+                (1 + KM2M * meshes[i].dep2 / celeri.RADIUS_EARTH)
+                - (1 + KM2M * meshes[i].dep1 / celeri.RADIUS_EARTH),
+            ]
+        )
+        tri_leg2 = np.transpose(
+            [
+                np.deg2rad(meshes[i].lon3 - meshes[i].lon1),
+                np.deg2rad(meshes[i].lat3 - meshes[i].lat1),
+                (1 + KM2M * meshes[i].dep3 / celeri.RADIUS_EARTH)
+                - (1 + KM2M * meshes[i].dep1 / celeri.RADIUS_EARTH),
+            ]
+        )
         meshes[i].nv = np.cross(tri_leg1, tri_leg2)
-        azimuth, elevation, r = celeri.cart2sph(meshes[i].nv[:,0], meshes[i].nv[:,1], meshes[i].nv[:,2])
+        azimuth, elevation, r = celeri.cart2sph(
+            meshes[i].nv[:, 0], meshes[i].nv[:, 1], meshes[i].nv[:, 2]
+        )
         meshes[i].strike = celeri.wrap2360(-np.rad2deg(azimuth))
         meshes[i].dip = 90 - np.rad2deg(elevation)
         meshes[i].dip_flag = meshes[i].dip != 90
@@ -150,11 +171,13 @@ def sph2cart(lon, lat, radius):
     z = radius * np.sin(np.deg2rad(lat))
     return x, y, z
 
+
 def cart2sph(x, y, z):
     azimuth = np.arctan2(y, x)
-    elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
-    r = np.sqrt(x**2 + y**2 + z**2)
+    elevation = np.arctan2(z, np.sqrt(x ** 2 + y ** 2))
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
     return azimuth, elevation, r
+
 
 def wrap2360(lon):
     lon[np.where(lon < 0.0)] += 360.0
