b0f29194b15326d4fa687f55ac76001f5807bf95
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-09-24 13:54:13 -0400

    Still working on comparing okada and cutde.  Working on gridded solutions to debug but stuck on moving past disp_mat.

--- celeri.py
+++ celeri.py
@@ -2171,6 +2171,7 @@ def test_end2end():
     assembly, operators.slip_rate_constraints = celeri.slip_rate_constraints(
         assembly, segment, block, command
     )
+    # TODO: Compute TDE partials: check with npy file like block closure
 
 
 def test_global_closure():
@@ -2200,3 +2201,106 @@ def test_global_closure():
         all_edge_idxs_stored = np.load(f)
 
     assert np.allclose(all_edge_idxs, all_edge_idxs_stored)
+
+
+def test_okada_equals_cutde():
+    # Observation coordinates
+    x_obs = np.array([2.0])
+    y_obs = np.array([1.0])
+    z_obs = np.array([0.0])
+    x_obs_vec = np.linspace(-5.0, 5.0, 10)
+    y_obs_vec = np.linspace(-5.0, 5.0, 10)
+    x_obs_mat, y_obs_mat = np.meshgrid(x_obs_vec, y_obs_vec)
+    x_obs = x_obs_mat.flatten()
+    y_obs = y_obs_mat.flatten()
+    z_obs = np.zeros_like(x_obs)
+
+    # Storage for displacements
+    u_x_okada = np.zeros_like(x_obs)
+    u_y_okada = np.zeros_like(y_obs)
+    u_z_okada = np.zeros_like(z_obs)
+    u_x_cutde = np.zeros_like(x_obs)
+    u_y_cutde = np.zeros_like(y_obs)
+    u_z_cutde = np.zeros_like(z_obs)
+
+    # Material properties
+    material_lambda = 3e10
+    material_mu = 3e10
+    poissons_ratio = material_lambda / (2 * (material_lambda + material_mu))
+    alpha = (material_lambda + material_mu) / (material_lambda + 2 * material_mu)
+
+    # Fault slip
+    strike_slip = 1
+    dip_slip = 0
+    tensile_slip = 0
+
+    # Parameters for Okada
+    segment_locking_depth = 1.0
+    segment_dip = 90
+    segment_length = 0.5
+    segment_up_dip_width = segment_locking_depth
+
+    # Okada
+    for i in range(x_obs.size):
+        _, u, _ = okada_wrapper.dc3dwrapper(
+            alpha,  # (lambda + mu) / (lambda + 2 * mu)
+            [
+                x_obs[i],
+                y_obs[i],
+                z_obs[i],
+            ],  # (meters) observation point
+            -segment_locking_depth,  # (meters) depth of the fault origin
+            segment_dip,  # (degrees) the dip-angle of the rectangular dislocation surface
+            [
+                -segment_length / 2,
+                segment_length / 2,
+            ],  # (meters) the along-strike range of the surface (al1,al2 in the original)
+            [
+                0,
+                segment_up_dip_width,
+            ],  # (meters) along-dip range of the surface (aw1, aw2 in the original)
+            [strike_slip, dip_slip, tensile_slip],
+        )  # (meters) strike-slip, dip-slip, tensile-slip
+        u_x_okada[i] = u[0]
+        u_y_okada[i] = u[1]
+        u_z_okada[i] = u[2]
+
+    # cutde
+    tri_x1 = np.array([-0.5, -0.5])
+    tri_y1 = np.array([0, 0])
+    tri_z1 = np.array([0, 0])
+    tri_x2 = np.array([0.5, 0.5])
+    tri_y2 = np.array([0, 0.0])
+    tri_z2 = np.array([0, 1])
+    tri_x3 = np.array([0.5, -0.5])
+    tri_y3 = np.array([0, 0])
+    tri_z3 = np.array([1, 1])
+
+    # Package coordinates for cutde call
+    obs_coords = np.vstack((x_obs, y_obs, np.zeros_like(x_obs))).T
+    tri_coords = np.array(
+        [[tri_x1, tri_y1, tri_z1], [tri_x2, tri_y2, tri_z2], [tri_x3, tri_y3, tri_z3]]
+    ).astype(float)
+    tri_coords = np.transpose(tri_coords, (2, 0, 1))
+
+    # Call cutde, multiply by displacements, and package for the return
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        disp_mat = cutde_halfspace.disp_matrix(
+            obs_pts=obs_coords, tris=tri_coords, nu=poissons_ratio
+        )
+    slip = np.array([[strike_slip, dip_slip, tensile_slip]])
+    disp = disp_mat.reshape((-1, 3)).dot(
+        slip.flatten()
+    )  # TODO: #42 I think this and the line below are wrong???
+    u_cutde = np.sum(disp.reshape(-1, 3), axis=1)
+    # u_x_cutde[i] = 0
+    # u_y_cutde[i] = 0
+    # u_z_cutde[i] = 0
+
+    # TODO: Try grid of observation points
+    # TODO: Try positive z thing again
+    # TODO: Check sign of slip
+    print(disp_mat.shape)
+    print(disp.shape)
+    print(u_cutde.shape)
