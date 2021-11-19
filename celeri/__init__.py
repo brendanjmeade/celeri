0a6433f3da090ade9bcbeb558dfbbcb2fee38bb9
brendanjmeade <brendanjmeade@users.noreply.github.com>
2021-11-19 13:37:28 -0500

    Updated exported additional functions from celeri.py and removed unused code.

--- celeri/__init__.py
+++ celeri/__init__.py
@@ -10,6 +10,16 @@ from .celeri import (
     get_elastic_operators,
     get_all_mesh_smoothing_matrices,
     get_all_mesh_smoothing_matrices_simple,
+    get_block_rotation_operator,
+    get_global_float_block_rotation_operator,
+    block_constraints,
+    slip_rate_constraints,
+    get_fault_slip_rate_partials,
+    get_strain_rate_centroid_operator,
+    get_mogi_operator,
+    post_process_estimation,
+    plot_input_summary,
+    plot_estimation_summary,
 )
 
 try:
