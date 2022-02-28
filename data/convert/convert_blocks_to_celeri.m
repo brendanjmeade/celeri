ab703fda4251b10eb3938d4561fc70d2f6b54fcb
brendanjmeade <brendanjmeade@gmail.com>
2022-02-28 10:26:36 -0500

    Start of data folder reorganization

--- data/convert/convert_blocks_to_celeri.m
+++
@@ -1,6 +0,0 @@
-function convert_blocks_to_celeri(command_file_name)
-command = ReadCommand(command_file_name);
-command2json(command_file_name);
-segment2csv(command.segFileName);
-block2csv(command.blockFileName);
-stadata2csv(command.staFileName);
