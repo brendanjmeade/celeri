977e6a317c8c433b0623bd538ab2d8729a3e2801
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-05-16 17:28:02 -0400

    Basic block and segment data.  Table colors are now variables

--- notebooks/celeri_report.py
+++ notebooks/celeri_report.py
@@ -12,20 +12,23 @@ import celeri
 
 
 def main(args: Dict):
-    # Process arguments
-    # Conditions for report of a single run
-    if (args.report_type == "report") or (args.report_type == None):
-        print("Reporting on single run.")
-        if args.folder_name_1 == None:
-            print("No folder specified.  Selecting most recent run folder.")
-            list_of_folders = filter(os.path.isdir, glob.glob("./../runs/*"))
-            list_of_folders = sorted(list_of_folders, key=os.path.getmtime)
-            folder_name_1 = list_of_folders[-1]
-            print(f"Most recent run folder is: {folder_name_1}.")
-        # TODO: If condition when folder_name_1 is specified
+    # Case 1: No arguments passed.  Assume most recent run folder and report on it.
+    if args.folder_name_1 == None:
+        print("No folder specified.  Selecting most recent run folder.")
+        list_of_folders = filter(os.path.isdir, glob.glob("./../runs/*"))
+        list_of_folders = sorted(list_of_folders, key=os.path.getmtime)
+        folder_name_1 = list_of_folders[-1]
+        print(f"Most recent run folder is: {folder_name_1}.")
+    # TODO: If condition when folder_name_1 is specified
 
-    elif args.report_type == "diff":
+    # Case 2: One argument passed.  Report on this folder.
+    elif args.folder_name_1 != None:
+        print("Reporting on single run.")
+
+    # Case 3: Two arguments passed.  Report on diff between these folders.
+    elif (args.folder_name_1 != None) and (args.folder_name_2 != None):
         print("Reporting on difference between two runs.")
+        pass
 
     else:
         print("Invalid comingation of arguments.  See celeri_report --help.")
@@ -37,9 +40,41 @@ def main(args: Dict):
     station_file_name_1 = glob.glob(os.path.join(folder_name_1, "model_station.csv"))[0]
     command_1 = celeri.get_command(command_file_name_1)
 
-    # Check for empty los file.  This is common
+    # Check for empty los file.  This is common.
     if command_1.los_file_name == {}:
         command_1.los_file_name = "none"
+
+    # Modify file names to read from `run` rather than `data` folder
+    # because they could have changed in `data` folder
+    command_1.station_file_name = os.path.join(
+        folder_name_1, os.path.basename(command_1.station_file_name)
+    )
+    command_1.segment_file_name = os.path.join(
+        folder_name_1, os.path.basename(command_1.segment_file_name)
+    )
+    command_1.block_file_name = os.path.join(
+        folder_name_1, os.path.basename(command_1.block_file_name)
+    )
+    command_1.los_file_name = os.path.join(
+        folder_name_1, os.path.basename(command_1.los_file_name)
+    )
+
+    data_1 = addict.Dict()
+    (
+        data_1.segment,
+        data_1.block,
+        data_1.meshes,
+        data_1.station,
+        data_1.mogi,
+        data_1.sar,
+    ) = celeri.read_data(command_1)
+
+    n_slip_rate_constraints_1 = (
+        np.count_nonzero(data_1.segment.ss_rate_flag)
+        + np.count_nonzero(data_1.segment.ds_rate_flag)
+        + np.count_nonzero(data_1.segment.ts_rate_flag)
+    )
+    n_block_constraints_1 = np.count_nonzero(data_1.block.apriori_flag, axis=0)
 
     # Calculation basic velocity statistics
     station_1 = pd.read_csv(station_file_name_1)
@@ -51,59 +86,82 @@ def main(args: Dict):
     mae_1 = np.mean(np.abs(station_1_residuals))
     mse_1 = np.mean(station_1_residuals ** 2.0)
 
+    # Reference colors
+    color_1 = "cyan"
+    color_2 = "yellow"
+    color_same = "green"
+    color_diff = "red"
+
     # Build table for reporting on a single model run
     console = Console()
     table = Table(show_header=True, header_style="bold #ffffff")
     table.add_column("property", justify="left")
     table.add_column("value", justify="left")
     table.add_row(
-        "[bold white]run folder",
-        f"[bold green]{os.path.basename(folder_name_1)}",
+        "[white]run folder",
+        f"[{color_1}]{os.path.basename(folder_name_1)}",
     )
     table.add_row(
-        "[bold white]command file",
-        f"[bold green]{os.path.basename(command_file_name_1)}",
+        "[white]command file",
+        f"[{color_1}]{os.path.basename(command_file_name_1)}",
     )
     table.add_row(
-        "[bold white]velocity file",
-        f"[bold green]{os.path.basename(command_1.station_file_name)}",
+        "[white]velocity file",
+        f"[{color_1}]{os.path.basename(command_1.station_file_name)}",
     )
     table.add_row(
-        "[bold white]segment file",
-        f"[bold green]{os.path.basename(command_1.segment_file_name)}",
+        "[white]segment file",
+        f"[{color_1}]{os.path.basename(command_1.segment_file_name)}",
     )
     table.add_row(
-        "[bold white]block file",
-        f"[bold green]{os.path.basename(command_1.block_file_name)}",
+        "[white]block file",
+        f"[{color_1}]{os.path.basename(command_1.block_file_name)}",
     )
     table.add_row(
-        "[bold white]los file",
-        f"[bold green]{os.path.basename(command_1.los_file_name)}",
-    )
-    table.add_row(
-        "[bold white]# stations",
-        f"[bold green]{len(station_1)}",
-    )
-    table.add_row(
-        "[bold white]# velocities",
-        f"[bold green]{2 * len(station_1)}",
-    )
-    table.add_row(
-        "[bold white]MAE",
-        f"[bold green]{mae_1:0.2f} (mm/yr) -- unweighted",
-    )
-    table.add_row(
-        "[bold white]MSE",
-        f"[bold green]{mse_1:0.2f} (mm/yr)^2 -- unweighted",
+        "[white]los file",
+        f"[{color_1}]{os.path.basename(command_1.los_file_name)}",
     )
 
-    # Weighted residual velocity (this is actually minimized)
+    # Velocity information
+    table.add_row(
+        "[white]# stations",
+        f"[{color_1}]{len(station_1)}",
+    )
+    table.add_row(
+        "[white]# velocities",
+        f"[{color_1}]{2 * len(station_1)}",
+    )
 
-    # Number of segments
-    # Number of slip rate constraints
+    # Block information
+    table.add_row(
+        "[white]# blocks",
+        f"[{color_1}]{len(data_1.block)}",
+    )
+    table.add_row(
+        "[white]# block constraints",
+        f"[{color_1}]{n_block_constraints_1}",
+    )
 
-    # Number of blocks
-    # Number of block motion constraints
+    # Segment information
+    table.add_row(
+        "[white]# segments",
+        f"[{color_1}]{len(data_1.segment)}",
+    )
+    table.add_row(
+        "[white]# slip rate constraints",
+        f"[{color_1}]{n_slip_rate_constraints_1}",
+    )
+
+    # Goodness of fit metrics
+    # TODO: Weighted residual velocity (this is actually minimized)
+    table.add_row(
+        "[white]MAE",
+        f"[cyan]{mae_1:0.2f} (mm/yr) -- unweighted",
+    )
+    table.add_row(
+        "[white]MSE",
+        f"[cyan]{mse_1:0.2f} (mm/yr)^2 -- unweighted",
+    )
 
     console.print(table)
 
@@ -113,13 +171,6 @@ def main(args: Dict):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--report_type",
-        type=str,
-        default=None,
-        help="'report' or 'diff'.  'report' will provide statstics on a single model run while 'diff' will compare two model runs.  If omited 'report' witll be assumed.",
-        required=False,
-    )
     parser.add_argument(
         "--folder_name_1",
         type=str,
@@ -132,7 +183,7 @@ if __name__ == "__main__":
         type=str,
         default=None,
         required=False,
-        help="Name of of folder 2.  Include for 'diff' option",
+        help="Name of of folder 2.",
     )
 
     args = addict.Dict(vars(parser.parse_args()))
