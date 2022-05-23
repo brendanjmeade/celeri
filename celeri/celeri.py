7471c4cf7d83502a711121a7057261691994aac4
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-05-23 14:31:27 -0400

    Adding command line flag for saving elastic partials

--- celeri/celeri.py
+++ celeri/celeri.py
@@ -838,20 +838,20 @@ def euler_pole_covariance_to_rotation_vector_covariance(
         else:
             # Calculate the partial derivatives
             dlat_dx = (
-                -z / (x**2 + y**2) ** (3 / 2) / (1 + z**2 / (x**2 + y**2)) * x
+                -z / (x ** 2 + y ** 2) ** (3 / 2) / (1 + z ** 2 / (x ** 2 + y ** 2)) * x
             )
             dlat_dy = (
-                -z / (x**2 + y**2) ** (3 / 2) / (1 + z**2 / (x**2 + y**2)) * y
+                -z / (x ** 2 + y ** 2) ** (3 / 2) / (1 + z ** 2 / (x ** 2 + y ** 2)) * y
             )
             dlat_dz = (
-                1 / (x**2 + y**2) ** (1 / 2) / (1 + z**2 / (x**2 + y**2))
+                1 / (x ** 2 + y ** 2) ** (1 / 2) / (1 + z ** 2 / (x ** 2 + y ** 2))
             )
-            dlon_dx = -y / x**2 / (1 + (y / x) ** 2)
+            dlon_dx = -y / x ** 2 / (1 + (y / x) ** 2)
             dlon_dy = 1 / x / (1 + (y / x) ** 2)
             dlon_dz = 0
-            dmag_dx = x / np.sqrt(x**2 + y**2 + z**2)
-            dmag_dy = y / np.sqrt(x**2 + y**2 + z**2)
-            dmag_dz = z / np.sqrt(x**2 + y**2 + z**2)
+            dmag_dx = x / np.sqrt(x ** 2 + y ** 2 + z ** 2)
+            dmag_dy = y / np.sqrt(x ** 2 + y ** 2 + z ** 2)
+            dmag_dz = z / np.sqrt(x ** 2 + y ** 2 + z ** 2)
             euler_to_cartsian_operator = np.array(
                 [
                     [dlat_dx, dlat_dy, dlat_dz],
@@ -1636,13 +1636,13 @@ def mogi_forward(mogi_lon, mogi_lat, mogi_depth, poissons_ratio, obs_lon, obs_la
             (1 - poissons_ratio)
             / np.pi
             * mogi_depth
-            / ((source_to_obs_distance**2.0 + mogi_depth**2) ** 1.5)
+            / ((source_to_obs_distance ** 2.0 + mogi_depth ** 2) ** 1.5)
         )
         u_radial = (
             (1 - poissons_ratio)
             / np.pi
             * source_to_obs_distance
-            / ((source_to_obs_distance**2 + mogi_depth**2.0) ** 1.5)
+            / ((source_to_obs_distance ** 2 + mogi_depth ** 2.0) ** 1.5)
         )
 
         # Convert radial displacement to east and north components
@@ -3071,7 +3071,7 @@ def get_weighting_vector(command, station, meshes, index):
         + index.n_tde_constraints_total
     )
     weighting_vector[index.start_station_row : index.end_station_row] = interleave2(
-        1 / (station.east_sig**2), 1 / (station.north_sig**2)
+        1 / (station.east_sig ** 2), 1 / (station.north_sig ** 2)
     )
     weighting_vector[
         index.start_block_constraints_row : index.end_block_constraints_row
@@ -3102,7 +3102,7 @@ def get_weighting_vector_single_mesh_for_col_norms(
     )
 
     weighting_vector[0 : 2 * index.n_stations] = interleave2(
-        1 / (station.east_sig**2), 1 / (station.north_sig**2)
+        1 / (station.east_sig ** 2), 1 / (station.north_sig ** 2)
     )
 
     weighting_vector[
@@ -3658,13 +3658,16 @@ def get_h_matrices_for_tde_meshes(
         weighting_vector_no_zero_rows = get_weighting_vector_single_mesh_for_col_norms(
             command, station, meshes, index, i
         )
-        current_tde_mesh_columns_full_no_zero_rows = np.vstack(
-            (
-                -tde_to_velocities,
-                operators.smoothing_matrix[i].toarray(),
-                operators.tde_slip_rate_constraints[i].toarray(),
+        current_tde_mesh_columns_full_no_zero_rows = (
+            np.vstack(
+                (
+                    -tde_to_velocities,
+                    operators.smoothing_matrix[i].toarray(),
+                    operators.tde_slip_rate_constraints[i].toarray(),
+                )
             )
-        ) * np.sqrt(weighting_vector_no_zero_rows[:, None])
+            * np.sqrt(weighting_vector_no_zero_rows[:, None])
+        )
 
         # Concatenate everthing we need for col_norms
         col_norms_current_tde_mesh = np.linalg.norm(
@@ -4483,7 +4486,7 @@ def plot_estimation_summary(
     )
     mean_average_error = np.mean(np.abs(residual_velocity_vector))
     mean_squared_error = (
-        np.sum(residual_velocity_vector**2.0) / residual_velocity_vector.size
+        np.sum(residual_velocity_vector ** 2.0) / residual_velocity_vector.size
     )
 
     # Create histogram of residual velocities
@@ -4702,11 +4705,18 @@ def parse_args():
         help="Flag for saving summary plot of model results (0 | 1)",
     )
     parser.add_argument(
+        "--save_elastic",
+        type=int,
+        default=0,
+        required=False,
+        help="Flag for saving elastic calculations (0 | 1)",
+    )
+    parser.add_argument(
         "--reuse_elastic",
         type=int,
-        default=None,
+        default=0,
         required=False,
-        help="Flag for reusinging elastic calculations (0 | 1)",
+        help="Flag for reusing elastic calculations (0 | 1)",
     )
     parser.add_argument(
         "--atol",
@@ -4963,7 +4973,7 @@ def get_weighting_vector_no_meshes(command, station, index):
         + index.n_slip_rate_constraints
     )
     weighting_vector[index.start_station_row : index.end_station_row] = interleave2(
-        1 / (station.east_sig**2), 1 / (station.north_sig**2)
+        1 / (station.east_sig ** 2), 1 / (station.north_sig ** 2)
     )
     weighting_vector[
         index.start_block_constraints_row : index.end_block_constraints_row
