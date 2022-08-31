ad58f4e2b60c19aa3031b67907a2d6310ea424a0
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-08-31 12:06:28 -0400

    Add area calculation for each element of TDE mesh at mesh read in time.

--- celeri/celeri.py
+++ celeri/celeri.py
@@ -199,6 +199,19 @@ def read_data(command: Dict):
                     RADIUS_EARTH + KM2M * meshes[i].dep3,
                 )
 
+                # Calcuate areas of each triangle in mesh
+                triangle_vertex_array = np.zeros((meshes[i].n_tde, 3, 3))
+                triangle_vertex_array[:, 0, 0] = meshes[i].x1
+                triangle_vertex_array[:, 1, 0] = meshes[i].x2
+                triangle_vertex_array[:, 2, 0] = meshes[i].x3
+                triangle_vertex_array[:, 0, 1] = meshes[i].y1
+                triangle_vertex_array[:, 1, 1] = meshes[i].y2
+                triangle_vertex_array[:, 2, 1] = meshes[i].y3
+                triangle_vertex_array[:, 0, 2] = meshes[i].z1
+                triangle_vertex_array[:, 1, 2] = meshes[i].z2
+                triangle_vertex_array[:, 2, 2] = meshes[i].z3
+                meshes[i].areas = triangle_area(triangle_vertex_array)
+
                 # Cartesian triangle centroids
                 meshes[i].x_centroid = (
                     meshes[i].x1 + meshes[i].x2 + meshes[i].x3
