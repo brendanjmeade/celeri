27191cb9fc46665c084b4dc2568d46467a566d40
Ben Mares <services-git-throwaway1@tensorial.com>
2025-03-26 11:30:08 +0100

    Add unoptimized okada_wrapper replacement

--- celeri/celeri_util.py
+++ celeri/celeri_util.py
@@ -1,4 +1,7 @@
+import warnings
+
 import numpy as np
+from cutde.halfspace import disp_matrix
 
 
 def sph2cart(lon, lat, radius):
@@ -15,3 +18,78 @@ def cart2sph(x, y, z):
     elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
     r = np.sqrt(x**2 + y**2 + z**2)
     return azimuth, elevation, r
+
+
+def dc3dwrapper_cutde_disp(alpha, xo, depth, dip, strike_width, dip_width, dislocation):
+    """Compute the Okada displacement vector using the cutde library.
+
+    The following should be equivalent up to floating point errors:
+
+    ```python
+    _, u, _ = okada_wrapper.dc3dwrapper(
+        alpha, xo, depth, dip, strike_width, dip_width, dislocation
+    )
+    u = dc3dwrapper_cutde_disp(
+        alpha, xo, depth, dip, strike_width, dip_width, dislocation
+    )
+    ```
+    """
+    # alpha = (lambda + mu) / (lambda + 2 mu)
+    # poissons_ratio = lambda / 2(lambda + mu)
+    poissons_ratio = 1 - 1 / (2 * alpha)
+    obs_pts = np.array([xo])
+
+    # Vertices of the rectangle
+    dip_rad = np.deg2rad(dip)
+    bl = [
+        strike_width[0],
+        dip_width[0] * np.cos(dip_rad),
+        dip_width[0] * np.sin(dip_rad) - depth,
+    ]
+    br = [
+        strike_width[1],
+        dip_width[0] * np.cos(dip_rad),
+        dip_width[0] * np.sin(dip_rad) - depth,
+    ]
+    tr = [
+        strike_width[1],
+        dip_width[1] * np.cos(dip_rad),
+        dip_width[1] * np.sin(dip_rad) - depth,
+    ]
+    tl = [
+        strike_width[0],
+        dip_width[1] * np.cos(dip_rad),
+        dip_width[1] * np.sin(dip_rad) - depth,
+    ]
+    tris = np.array(
+        [[tr, bl, br], [bl, tr, tl]]
+    )  # Oriented counterclockwise looking down
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            message="The obs_pts input array has Fortran ordering. "
+            "Converting to C ordering. This may be expensive.",
+            category=UserWarning,
+        )
+        # This warning is false. The array is C-contiguous. It is also F-contiguous.
+        # But instead of correctly checking for C-contiguity, it checks incorrectly
+        # for Fortran-noncontiguity:
+        # # assert not obs_pts.flags.f_contiguous
+        assert obs_pts.flags.c_contiguous
+        assert tris.flags.c_contiguous
+        disp_mat = disp_matrix(obs_pts=obs_pts, tris=tris, nu=poissons_ratio)
+
+    # (obs, space, triangle, slip)
+    assert disp_mat.shape == (1, 3, 2, 3)
+
+    slip_vec = np.array(dislocation)  # (strike, dip, open)
+    assert slip_vec.shape == (3,)
+
+    # Sum over the triangle axis and contract the slip axis with the slip vector.
+    disp_vec = np.einsum("ijkl,l->ij", disp_mat, slip_vec)
+    assert disp_vec.shape == (1, 3)  # (obs, space)
+
+    disp_vec0 = disp_vec[0]
+    assert disp_vec0.shape == (3,)  # (space,)
+    return disp_vec0
