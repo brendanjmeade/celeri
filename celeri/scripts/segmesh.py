d1fab6acd3c35b4db00cf8b2c763529e7f250cd9
Brendan Meade <brendanjmeade@gmail.com>
2025-07-17 14:47:17 -0400

    Renaming patch_file_name to mesh_file_name and patch_flag to mesh_flag.

--- celeri/scripts/segmesh.py
+++ celeri/scripts/segmesh.py
@@ -245,17 +245,17 @@ def main():
             gmsh.finalize()
 
             # Update segment DataFrame
-            # patch_file_name (really an integer) may differ from the _ribbonmesh number
-            # patch_file_name is really an index into the list of meshes in the mesh_param
-            # model.segment.patch_file_name[this_seg_mesh_idx] = (
+            # mesh_file_name (really an integer) may differ from the _ribbonmesh number
+            # mesh_file_name is really an index into the list of meshes in the mesh_param
+            # model.segment.mesh_file_name[this_seg_mesh_idx] = (
             #     n_meshes + i
             # )  # 0-based indexing means we start at n_meshes
-            # model.segment.patch_flag[this_seg_mesh_idx] = 1
+            # model.segment.mesh_flag[this_seg_mesh_idx] = 1
             # model.segment.create_ribbon_mesh[this_seg_mesh_idx] = 0
-            model.segment.loc[this_seg_mesh_idx, "patch_file_name"] = (
+            model.segment.loc[this_seg_mesh_idx, "mesh_file_name"] = (
                 n_meshes + i
             )  # 0-based indexing means we start at n_meshes
-            model.segment.loc[this_seg_mesh_idx, "patch_flag"] = 1
+            model.segment.loc[this_seg_mesh_idx, "mesh_flag"] = 1
             model.segment.loc[this_seg_mesh_idx, "create_ribbon_mesh"] = 0
 
             # Print status
