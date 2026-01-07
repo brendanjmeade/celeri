64c6124c7b6456535830565ad9c8ea54c7ba7e92
elc45 <eliot.carlson@pymc-labs.com>
2026-01-07 12:25:58 -0500

    add vertical vel data to likelihood

--- celeri/solve_mcmc.py
+++ celeri/solve_mcmc.py
@@ -506,7 +506,11 @@ def _add_station_velocity_likelihood(model: Model, mu):
     import pymc as pm
 
     sigma = pm.HalfNormal("sigma", sigma=2)
-    data = np.array([model.station.east_vel, model.station.north_vel]).T
+
+    if model.config.include_vertical_velocity:
+        data = np.array([model.station.east_vel, model.station.north_vel, model.station.up_vel]).T
+    else:
+        data = np.array([model.station.east_vel, model.station.north_vel]).T
 
     lh_dist = pm.StudentT.dist
 
@@ -555,6 +559,8 @@ def _add_station_velocity_likelihood(model: Model, mu):
             f"{(areas >= effective_area).sum()}"
         )
 
+        dims = ("station", "xyz") if model.config.include_vertical_velocity else ("station", "xy")
+
         pm.CustomDist(
             "station_velocity",
             weight[:, None],
@@ -563,7 +569,7 @@ def _add_station_velocity_likelihood(model: Model, mu):
             logp=lh,
             random=random,
             observed=data,
-            dims=("station", "xy"),
+            dims=dims,
         )
     else:
         raise ValueError(
@@ -723,8 +729,13 @@ def _build_pymc_model(model: Model, operators: Operators) -> PymcModel:
             + mogi_velocity
             + elastic_velocity
         )
-        mu = mu.reshape((len(model.station), 3))[:, :2]
-        mu_det = pm.Deterministic("mu", mu, dims=("station", "xy"))
+        mu = mu.reshape((len(model.station), 3))
+        
+        if not model.config.include_vertical_velocity:
+            mu = mu[:, :2]
+
+        dims = ("station", "xyz") if model.config.include_vertical_velocity else ("station", "xy")
+        mu_det = pm.Deterministic("mu", mu, dims=dims)
         _add_station_velocity_likelihood(model, mu_det)
         _add_segment_constraints(model, operators, rotation)
 
