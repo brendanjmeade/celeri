eff45e0328563537a5f95253f5fa2ae83f5ab01c
Jack Loveless <jloveles@smith.edu>
2026-05-21 14:09:40 -0400

    Minor changes to Matlab conversion scripts

--- convert/block2csv.m
+++ convert/block2csv.m
@@ -12,11 +12,11 @@ block.euler_lat = block.eulerLat;
 block.euler_lat_sig = block.eulerLatSig;
 block.rotation_rate = block.rotationRate;
 block.rotation_rate_sig = block.rotationRateSig;
-block.rotation_flag = block.rotationInfo;
-block.apriori_flag = block.aprioriTog;
+block.rotation_flag = block.aprioriTog;
+block.apriori_flag = zeros(numel(block.interior_lon), 1);
 block.strain_rate = zeros(numel(block.interior_lon), 1);
 block.strain_rate_sig = zeros(numel(block.interior_lon), 1);
-block.strain_rate_flag = zeros(numel(block.interior_lon), 1);
+block.strain_rate_flag = block.rotationInfo;
 
 % Delete old field names
 block = rmfield(block, "interiorLon");
